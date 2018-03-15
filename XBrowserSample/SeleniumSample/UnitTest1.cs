9cdee5983e160f90f1c433f17a75e23aff54368c
Kavipriya Adhinarayanan <kaadhina@microsoft.com>
2018-03-15 14:05:51 +0530

    Making the test data driven

--- XBrowserSample/SeleniumSample/UnitTest1.cs
+++ XBrowserSample/SeleniumSample/UnitTest1.cs
@@ -5,6 +5,7 @@ using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.IE;
 using System;
+using OpenQA.Selenium.Remote;
 
 namespace SeleniumSample
 {
@@ -12,53 +13,77 @@ namespace SeleniumSample
     public class UnitTest1
     {
         [TestMethod]
-        //[DeploymentItem(@"..\..\Drivers\chromedriver.exe")]
-        public void ChromeTest()
+        [DataRow("chrome")]
+        [DataRow("firefox")]
+        [DataRow("ie")]
+        public void SearchPageTest(string browser)
+        {
+            var driver = GetDriver(browser);
+            driver.Navigate().GoToUrl("http://www.google.com");
+            driver.FindElement(By.Id("lst-ib")).SendKeys("Hello");
+            var filePath = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString()) + ".png";
+            var screenshot = driver.GetScreenshot();
+            screenshot.SaveAsFile(filePath);
+            TestContext.AddResultFile(filePath);
+            driver.FindElement(By.Id("lst-ib")).SendKeys(Keys.Enter);
+            driver.Quit();
+        }
+        
+        #region private methods
+
+        private RemoteWebDriver GetDriver(string browser)
+        {
+            switch (browser)
+            {
+                case "chrome":
+                    return GetChromeDriver();
+                case "firefox":
+                    return GetFirefoxDriver();
+                case "ie":
+                default:
+                    return GetIEDriver();
+            }
+        }
+
+        private RemoteWebDriver GetChromeDriver()
         {
             var path = Environment.GetEnvironmentVariable("ChromeSeleniumDriverPath");
-            var chromeDriver = new ChromeDriver(path);
-            chromeDriver.Navigate().GoToUrl("http://www.google.com");
-            chromeDriver.FindElement(By.Id("lst-ib")).SendKeys("Hello");
-            var filePath = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString()) + ".png";
-            var screenshot = chromeDriver.GetScreenshot();
-            screenshot.SaveAsFile(filePath);
-            TestContext.AddResultFile(filePath);
-            chromeDriver.FindElement(By.Id("lst-ib")).SendKeys(Keys.Enter);
-            chromeDriver.Quit();
+            if (!string.IsNullOrWhiteSpace(path))
+            {
+                return new ChromeDriver(path);
+            }
+            else
+            {
+                return new ChromeDriver();
+            }
         }
 
-        [TestMethod]
-        //[DeploymentItem(@"..\..\Drivers\geckodriver.exe")]
-        public void FirefoxTest()
+        private RemoteWebDriver GetFirefoxDriver()
         {
             var path = Environment.GetEnvironmentVariable("GeckoSeleniumDriverPath");
-            var firefoxDriver = new FirefoxDriver(path);
-            firefoxDriver.Navigate().GoToUrl("http://www.google.com");
-            firefoxDriver.FindElement(By.Id("lst-ib")).SendKeys("Hello");
-            var filePath = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString()) + ".png";
-            Console.WriteLine(filePath);
-            var screenshot = firefoxDriver.GetScreenshot();
-            screenshot.SaveAsFile(filePath, ScreenshotImageFormat.Png);
-            TestContext.AddResultFile(filePath);
-            firefoxDriver.FindElement(By.Id("lst-ib")).SendKeys(Keys.Enter);
-            firefoxDriver.Quit();
+            if (!string.IsNullOrWhiteSpace(path))
+            {
+                return new FirefoxDriver(path);
+            }
+            else
+            {
+                return new FirefoxDriver();
+            }
         }
 
-        [TestMethod]
-        //[DeploymentItem(@"..\..\Drivers\IEDriverServer.exe")]
-        public void IETest()
+        private RemoteWebDriver GetIEDriver()
         {
-            var path= Environment.GetEnvironmentVariable("IESeleniumDriverPath");
-            var iedriver = new InternetExplorerDriver(path);
-            iedriver.Navigate().GoToUrl("http://www.google.com");
-            iedriver.FindElement(By.Id("lst-ib")).SendKeys("Hello");
-            var filePath = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString()) + ".png";
-            var screenshot = iedriver.GetScreenshot();
-            screenshot.SaveAsFile(filePath);
-            TestContext.AddResultFile(filePath);
-            iedriver.FindElement(By.Id("lst-ib")).SendKeys(Keys.Enter);
-            iedriver.Quit();
+            var path = Environment.GetEnvironmentVariable("IESeleniumDriverPath");
+            if (!string.IsNullOrWhiteSpace(path))
+            {
+                return new InternetExplorerDriver(path);
+            }
+            else
+            {
+                return new InternetExplorerDriver();
+            }
         }
+        #endregion
         public TestContext TestContext { get; set; }
     }
 }
